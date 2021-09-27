6a6cc746ca8a4a8afbf0a43f5bc2ac52c4a43cbd
Brian de Reus <briandereus69@gmail.com>
2021-09-27 12:59:21 +0200

    adding files

---
+++ PYTB1L2PoemPrinting/GedichtBrianPY.py
@@ -0,0 +1,14 @@
+verse1 = "Mijn voornaam is Brian en mijn achternaam de Reus"
+verse2 = "ik ben zwaar depressief want ik had geen keus"
+verse3 = "god zei tegen mij, hier, neem ADHD"
+verse4 = "ik ga je niet meer helpen, je doet het er maar mee"
+verse5 = "en natuurlijk, denk je soms aan om jezelf te vermoorden"
+verse6 = "boeie, ik zorg er voor dat je niet high of dronken kan worden!"
+verse7 = "En dan kom ik altijd te laat"
+verse8 = "en dan denkt de leraar, laten we hopen dat hij rustig is alleen om er dan achter te komen dat ik tering veel praat"
+verse9 = "gelukkig kan ik goed gamen, drummen en gitaar spelen"
+verse10 = "maar nadat ik dat doe voor meer dan 30 minuten begin ik me alweer te-vervelen"
+verse11 = "kort om, life is fucked en ik wil niet meer leven"
+verse12 = "maar ik weet dat dit niet voor altijd blijft, het is maar voor even"
+
+print(verse1,verse2,verse3,verse4,verse5,verse6,verse7,verse8,verse9,verse10,verse11,verse12)
