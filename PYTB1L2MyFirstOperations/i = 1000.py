ef0e0c037a41432fde0a355847f6b8875ed1875c
Brian de Reus <briandereus69@gmail.com>
2021-10-12 22:25:31 +0200

    Badeendje

--- PYTB1L2MyFirstOperations/i = 1000.py
+++ PYTB1L2MyFirstOperations/i = 1000.py
@@ -1,9 +1,9 @@
-Banaan = 1000
-while Banaan < 6969:
-   print(Banaan)
-   if Banaan == 0:
-       break
-   Banaan -= 1
-aardappel = ("gay")
-
-print("Je moeder is:", aardappel)
+print("Je word wakker door het geluid van geklop, je doet de deur open. Het is de politie, wat doe je ?")
+print("typ: meegaan, of rennen")
+choice = input()
+if choice == 'meegaan':
+    print("De politie neemt je mee naar het bureau en stelt je vragen over je kat, Panter")
+elif choice == 'rennen':
+    print("Je rent weg, de politie vangt je en neemt je mee naar het bureau, hij stelt je vragen over je kat, Panter")
+else:
+    print(choice, "Dat is geen keuze")
